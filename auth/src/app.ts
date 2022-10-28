a9830876cd7f2719f6a4cd8f8faf9ccf44fd1141
Nishant <bhartirkmv@gmail.com>
2022-10-28 09:58:07 +0530

    changes made to infra

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -31,7 +31,8 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false
+    //secure: process.env.NODE_ENV !== 'test'
   })
 );
 app.use(currentUserRouter);
